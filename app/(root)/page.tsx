fa58ace55ed6b407207319c34c5889ef4ccce1ac
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-08-12 18:35:11 +0300

    Implement the left side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import { auth } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home = async () => {
   const session = await auth();
@@ -7,7 +9,7 @@ const Home = async () => {
 
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-inter">Welcome to the world of Next.js</h1>
     </>
   );
 };
