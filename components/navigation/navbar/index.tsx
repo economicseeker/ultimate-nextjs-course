459ec745e90b4743c3ba9d2dbb8acae4b52d8fce
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-08-12 16:58:36 +0300

    MobileNav and NavLinks

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import "@/app/globals.css";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
+
+import MobileNavigation from "./MobileNavigation";
 import { Theme } from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
@@ -19,10 +20,13 @@ const Navbar = () => {
           Dev<span className="text-primary-500">Flow</span>
         </p>
       </Link>
+
       <p>Global Search</p>
 
       <div className="flex-between gap-5">
         <Theme />
+
+        <MobileNavigation />
       </div>
     </nav>
   );
