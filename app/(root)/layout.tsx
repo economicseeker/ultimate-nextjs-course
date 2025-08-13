2e86cb89530730d01ff16ba44c659dd656410db4
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-08-13 12:04:08 +0300

    Implement RightSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
-import LeftSideBar from "@/components/navigation/LeftSideBar";
-import LeftSide from "@/components/navigation/LeftSideBar";
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -8,11 +8,13 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        <LeftSideBar />
+        <LeftSidebar />
 
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+
+        <RightSidebar />
       </div>
     </main>
   );
